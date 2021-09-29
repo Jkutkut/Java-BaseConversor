b5d70b3aa2c8abeea2bdfbc76edd44088f68b212
jkutkut <jorgeregonzalez@gmail.com>
2021-09-29 12:42:53 +0200

    Test names updated

--- src/Classes/ConverterTest.java
+++ src/Classes/ConverterTest.java
@@ -1,11 +1,11 @@
 package Classes;
 
-import Classes.Converter;
+//import Classes.Converter;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class ConverterTest {
+	
 
 	@Test
 	void testDecimal2Binary() throws Exception {
@@ -13,7 +13,7 @@ class ConverterTest {
 			{"10", "1010"},
 			{"12323", "11000000100011"},
 			{"128", "10000000"},
-			{"35", "100101"},
+			{"35", "100011"},
 			{"46", "101110"},
 			{"322", "101000010"}
 		};
@@ -29,16 +29,15 @@ class ConverterTest {
 			{"101011110101", "2805"},
 			{"11110", "30"},
 			{"1010001110", "654"},
-			{"0101011110", "550"}
+			{"101011110", "350"}
 		};
 		this.executeTest(tests, Converter.BINARY, Converter.DECIMAL);	
 	}
 	
 	
 	//	Decimal-Octal
-	
 	@Test
-	void testDecimalOctal() throws Exception {
+	void testDecimal2Octal() throws Exception {
 		String[][] tests = {
 			{"124", "174"},
 			{"235", "353"},
@@ -49,93 +48,79 @@ class ConverterTest {
 		this.executeTest(tests, Converter.DECIMAL, Converter.OCTAL);
 	}
 	
+	
 	@Test
-	void testOctalDecimal() throws Exception {
+	void testOctal2Decimal() throws Exception {
 		String[][] tests = {
 			{"345", "229"},
 			{"123", "83"},
 			{"52", "42"},
 			{"67", "55"}
 		};
+		this.executeTest(tests, Converter.OCTAL, Converter.DECIMAL);
 	}
 	
 	
 	//	Decimal-Hexadecimal
 	
 	@Test
-	void testDecimalHexadecimal() throws Exception {
+	void testDecimal2Hexadecimal() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
+				{"231", "E7"},
+				{"89", "59"},
+				{"3456", "D80"},
+				{"12", "C"}
 		};
+		
+		this.executeTest(tests, Converter.DECIMAL, Converter.HEXADECIMAL);
 	}
 	
 	@Test
-	void testHexadecimalDecimal() throws Exception {
+	void testHexadecimal2Decimal() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
 		};
+		
+		this.executeTest(tests, Converter.HEXADECIMAL, Converter.DECIMAL);
 	}
 	
 	
 	// Hexadecimal-Octal
 	
 	@Test
-	void testHexadecimalOctal() throws Exception {
+	void testHexadecimal2Octal() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
+				{"15D5", "12725"},
+				{"3569", "32551"},
+				{"52BB", "51273"},
+				{"5769", "53551"}
 		};
+		
+		this.executeTest(tests, Converter.HEXADECIMAL, Converter.OCTAL);
 	}
 	
 	@Test
-	void testOctalHexadecimal() throws Exception {
+	void testOctal2Hexadecimal() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
 		};
+		
+		this.executeTest(tests, Converter.OCTAL, Converter.HEXADECIMAL);
 	}
 	
 	// Binario-Hexadecimal
 	
 	@Test
-	void testBinarioHexadecimal() throws Exception {
+	void testBinario2Hexadecimal() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
 		};
 	}
 	
 	@Test
-	void testHexadecimalBinariio() throws Exception {
+	void testHexadecimal2Binario() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
+				{"234", "1000110100"},
+				{"A234F", "10100010001101001111"},
+				{"3572", "11010101110010"},
+				{"3567", "10010101100111"}
 		};
 	}
 	
@@ -143,26 +128,22 @@ class ConverterTest {
 	// Binario-Octal
 	
 	@Test
-	void testBinarioOctal() throws Exception {
+	void testBinario2Octal() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
+				{"1010111010101", "12725"},
+				{"11010101101001", "32551"},
+				{"101001010111011", "51273"},
+				{"101011101101001", "51273"},
+				{"0101011101101001", "53551"}
 		};
 	}
 	
 	@Test
-	void testOctalBinario() throws Exception {
+	void testOctal2Binario() throws Exception {
 		String[][] tests = {
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""},
-				{"", ""}
+				{"234", "10011100"},
+				{"3572", "11101111010"},
+				{"3567", "11101110111"}
 		};
 	}
 	
@@ -172,7 +153,7 @@ class ConverterTest {
 		try {
 			for (; i < tests.length; i++) {
 				assertEquals(
-						Converter.converter(tests[i][0], baseFrom, baseTo, false),
+						Classes.Converter.converter(tests[i][0], baseFrom, baseTo, false),
 						tests[i][1]
 				);
 			}
